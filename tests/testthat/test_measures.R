0c960841ce25b366d05dc21a01cd989f6f8ed61a
bfakos <bfakosreg@gmail.com>
2021-12-06 15:56:13 +0100

    create testing skeleton

---
+++ tests/testthat/test_measures.R
@@ -0,0 +1,59 @@
+test_that(desc = "returns expexted value for normal parameters - default parameterization",
+					code = {
+						expect_equal(object = TRUE, expected = TRUE)
+					}
+)
+
+test_that(desc = "returns expexted value for normal parameters - goodness = TRUE, ROCR available",
+					code = {
+						expect_equal(object = TRUE, expected = TRUE)
+					}
+)
+
+test_that(desc = "returns expexted value for normal parameters - goodness = TRUE, ROCR unavailable",
+					code = {
+						expect_equal(object = TRUE, expected = TRUE)
+					}
+)
+
+test_that(desc = "returns expexted value for normal parameters - df = TRUE, for",
+					code = {
+						expect_equal(object = TRUE, expected = TRUE)
+					}
+)
+
+test_that(desc = "returns expexted value for normal parameters - df = TRUE, lapply()",
+					code = {
+						expect_equal(object = TRUE, expected = TRUE)
+					}
+)
+
+test_that(desc = "returns errors/warnings if needed - parameter: observations",
+					code = {
+						expect_equal(object = TRUE, expected = TRUE)
+					}
+)
+
+test_that(desc = "returns errors/warnings if needed - parameter: predictions",
+					code = {
+						expect_equal(object = TRUE, expected = TRUE)
+					}
+)
+
+test_that(desc = "returns errors/warnings if needed - parameter: evaluation_mask",
+					code = {
+						expect_equal(object = TRUE, expected = TRUE)
+					}
+)
+
+test_that(desc = "returns errors/warnings if needed - parameter: goodness",
+					code = {
+						expect_equal(object = TRUE, expected = TRUE)
+					}
+)
+
+test_that(desc = "returns errors/warnings if needed - parameter: df",
+					code = {
+						expect_equal(object = TRUE, expected = TRUE)
+					}
+)
