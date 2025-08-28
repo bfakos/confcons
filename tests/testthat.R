aff08c8e4d4dee2a04dd62436d63037c3c09479b
bfakos <bfakos@gmail.com>
2025-08-28 07:20:38 +0200

    handle missing packages needed for vignette

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,10 @@
-library(testthat)
-library(vctrs)
-library(confcons)
-requireNamespace("ROCR", quietly = TRUE)
+needed_packages <- c("testthat", "vctrs", "mockery", "ROCR", "confcons")
 
-test_check("confcons")
+missing_packages <- needed_packages[!vapply(X = needed_packages,
+																						FUN = requireNamespace,
+																						quietly = TRUE,
+																						FUN.VALUE = logical(1))]
+
+if (length(missing_packages) == 0) {
+	test_check("confcons")
+}
